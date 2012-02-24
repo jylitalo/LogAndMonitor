fed65733c40baf804c6f5028b3d8cb60af1b9c5d
root <root@vs2656.ams2.alvotech.de>
2012-02-24 19:01:28 +0100

    Added support for weekly backups, debug and verbose messaging.

--- Linux/backup.py
+++ Linux/backup.py
@@ -5,15 +5,23 @@ import sys
 import time
 
 if __name__ == '__main__':
+    debug = 1
+    verbose = 0
     dirs = "/bin /boot /etc /home /lib* /m* /opt /root /s* /tmp /usr /var"
-    newdir = "/backup/" + time.strftime('%Y-%m-%d')
+    newdir = "/backup/" + time.strftime('%Y-week-%W')
     if os.access(newdir, os.F_OK):
-        print newdir + " already exists. Doing exit."
-        sys.exit(1)
-    args = ["-aH", "-v"]
+        newdir = "/backup/" + time.strftime('%Y-%m-%d')
+        if os.access(newdir, os.F_OK):
+            print newdir + " already exists. Doing exit."
+            sys.exit(1)
+    args = ["-aH"]
+    if verbose: args.append("-v")
+    else: args.append("-q")
     if os.access("/backup/latest",os.R_OK):
         args.append("--link-dest=/backup/latest")
-    os.system("/usr/bin/rsync %s %s %s" % (" ".join(args),dirs,newdir))
+    cmdline = "/usr/bin/rsync %s %s %s" % (" ".join(args),dirs,newdir)
+    if debug: print "### " + cmdline
+    os.system(cmdline)
     if os.access("/backup/latest",os.F_OK):
         os.unlink("/backup/latest")
     os.symlink(newdir,"/backup/latest")
