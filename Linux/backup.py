efaae2865535dd04f05b53ca42a94f89a53acda1
root <root@vs2656.ams2.alvotech.de>
2012-03-30 21:58:39 +0300

    Added mysqldump into backup script.

--- Linux/backup.py
+++ Linux/backup.py
@@ -3,10 +3,12 @@
 import os
 import sys
 import time
+import ConfigParser
 
 if __name__ == '__main__':
     debug = 1
     verbose = 0
+    if debug or verbose: print "### " + time.ctime()
     dirs = "/bin /boot /etc /home /lib* /m* /opt /root /s* /tmp /usr /var"
     newdir = "/backup/" + time.strftime('%Y-week-%W')
     if os.access(newdir, os.F_OK):
@@ -14,7 +16,8 @@ if __name__ == '__main__':
         if os.access(newdir, os.F_OK):
             print newdir + " already exists. Doing exit."
             sys.exit(1)
-    args = ["-aH"]
+
+    args = ["-aH --exclude=/var/spool/pnp4nagios"]
     if verbose: args.append("-v")
     else: args.append("-q")
     if os.access("/backup/latest",os.R_OK):
@@ -22,6 +25,25 @@ if __name__ == '__main__':
     cmdline = "/usr/bin/rsync %s %s %s" % (" ".join(args),dirs,newdir)
     if debug: print "### " + cmdline
     os.system(cmdline)
+    if debug or verbose: print "### " + time.ctime()
+
+    fname = "/etc/backup.cf"
+    if os.access(fname, os.R_OK):
+        if debug: print "### mysqldump"
+        cf = ConfigParser.ConfigParser()
+        cf.read(fname)
+        username = cf.get("mysql","username")
+        password = cf.get("mysql","password")
+        newdump = "/backup/mysql/mysql-" + time.strftime('%d')
+        if os.access(newdump,os.F_OK): os.unlink(newdump)
+        args = ["-u " + username,"-p'%s'" % (password),"--all-databases","-r" + newdump]
+        if verbose: args.append("-v")
+        cmdline = "mysqldump " + " ".join(args)
+        os.system(cmdline)
+        if debug or verbose: print "### " + time.ctime()
+    elif debug:
+        print "### no configuration file for mysqldump"
+
     if os.access("/backup/latest",os.F_OK):
         os.unlink("/backup/latest")
     os.symlink(newdir,"/backup/latest")
