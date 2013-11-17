990112a1f7d81850624592caa2906b5d9e84f2fc
Juha Ylitalo <juha@ylitalot.net>
2013-11-17 19:42:30 +0200

    added getter and setter for changed the self._dir property

--- python/assets_on_octopress.py
+++ python/assets_on_octopress.py
@@ -7,21 +7,26 @@ import stat
 import sys
 
 class AssetsFinder(object):
-  def __init__(self,dir):
-    # Setup
+  def __init__(self):
+    self._dir = None
+    self.debug = False
+    self._found = []
+    self._linked = {}
+
+  @property
+  def dir(self): return self._dir
+
+  @dir.setter
+  def dir(self, dname):
     errors = []
     for key in ["_posts","images","assets"]:
-      if not os.path.isdir("%s/%s" % (dir,key)): errors.append(key)
+      if not os.path.isdir("%s/%s" % (dname,key)): errors.append(key)
     if errors:
       print("""Invalid directory for Octopress source.
 %s doesn't have following subdirectories: %s
 Doing exit.""" % (dir,", ".join(errors)))
       sys.exit(2)
-    # Execute
-    self._dir = dir
-    self.debug = False
-    self._found = []
-    self._linked = {}
+    self._dir = dname
 
   @staticmethod
   def _extract_from_markdown(line):
@@ -49,22 +54,23 @@ Doing exit.""" % (dir,", ".join(errors)))
 
   def _scan_tree(self,subdir):
     ret = []
-    ignore = len(self._dir)
-    for root,dirnames,filenames in os.walk(self._dir + subdir):
+    ignore = len(self.dir)
+    for root,dirnames,filenames in os.walk(self.dir + subdir):
       for filename in filenames:
         fname = os.path.join(root, filename)[ignore:]
         if not self._ignore(fname): self._validate_found(fname)
     return ret
 
-  def scan(self):
+  def scan(self,dir):
+    self.dir = dir
     begin_index = len("yyyy-mm-dd-")
     end_index = len(".markdown")
     markdown_files = []
-    for root,dirnames,filenames in os.walk(self._dir):
+    for root,dirnames,filenames in os.walk(dir):
       for fname in fnmatch.filter(filenames,"*.markdown"):
         markdown_files.append(os.path.join(root,fname))
     if not markdown_files:
-      print("### Unable to find any markdown files from " + self._dir)
+      print("### Unable to find any markdown files from " + dir)
       sys.exit(1)
     else:
       print("### processing %d markdown files" % (len(markdown_files)))
@@ -82,11 +88,7 @@ Doing exit.""" % (dir,", ".join(errors)))
             if link in self._linked: self._linked[link].append(url_name)
             else: self._linked[link] = [url_name]
       f.close()
-    print("### found %d links" % (len(self._linked)))
-    self._scan_tree("/assets/")
-    print("### found after /assets/ is %d"% (len(self._found)))
-    self._scan_tree("/images/")
-    print("### found after /images/ is %d"% (len(self._found)))
+    for key in ["assets","images"]: self._scan_tree("/" + key + "/")
 
   def _validate_found(self, fname): self._found.append(fname)
   def _validate_waste(self,fname): print("WASTE: '%s'" % (fname))
@@ -111,8 +113,8 @@ Doing exit.""" % (dir,", ".join(errors)))
       for fname in linked_keys: self._validate_missing(fname)
 
 class AssetsFixer(AssetsFinder):
-  def __init__(self,dir):
-    AssetsFinder.__init__(self,dir)
+  def __init__(self):
+    AssetsFinder.__init__(self)
     self._images_root = os.path.expanduser("~/kuvat/original_jpg")
     self._validate_original = True
     self._convert_missing = True
@@ -140,7 +142,7 @@ class AssetsFixer(AssetsFinder):
       print("### unable to find original file for " + fname)
       return False
     # Execute
-    full_fname = self._dir + fname
+    full_fname = self.dir + fname
     original_mtime = os.stat(original_fname)[stat.ST_MTIME]
     full_mtime = os.stat(full_fname)[stat.ST_MTIME]
     if original_mtime >= full_mtime:
@@ -152,8 +154,8 @@ class AssetsFixer(AssetsFinder):
       return True
 
   def _validate_waste(self,fname):
-    print("unlinking %s%s (validate waste)" % (self._dir,fname))
-    os.unlink("%s%s" % (self._dir,fname))
+    print("unlinking %s%s (validate waste)" % (self.dir,fname))
+    os.unlink("%s%s" % (self.dir,fname))
 
   def _resize_image(self,source_name,dest_name,resolution):
     cmdline = "convert -resize %s %s %s" % (resolution,source_name,dest_name)
@@ -169,7 +171,7 @@ class AssetsFixer(AssetsFinder):
     original_fname = self._original_image(fname)
     if not original_fname:
       print("### unable to find original_image for %s (references: %s)" % (fname,", ".join(self._linked[fname])))
-    fname = self._dir + fname
+    fname = self.dir + fname
     dname = os.path.dirname(fname)
     if not os.access(dname,os.F_OK): os.mkdir(dname)
     if fname.endswith("_t.jpg"):
@@ -187,6 +189,6 @@ if __name__ == '__main__':
     print("Usage: assets_on_octopress.py [dir]")
     sys.exit(4)
   dir = sys.argv[1]
-  assets = AssetsFixer(dir)
-  assets.scan()
+  assets = AssetsFixer()
+  assets.scan(dir)
   assets.validate()
