17b848ec9a8d646a503bdb7f2baf4a2882d2cf73
Juha Ylitalo <juha@ylitalot.net>
2013-11-24 20:15:52 +0200

    small script for generating photo-a-day posts into ylitalot.com

---
+++ python/padpost.py
@@ -0,0 +1,95 @@
+import glob
+import os
+import sys
+import time
+
+import exifdump
+import assets_on_octopress
+
+def process_file(fname):
+  fl = dt = None
+  f = open(fname)
+  data = f.read(12)
+  length = ord(data[4])*256 + ord(data[5])
+  data = f.read(length-8)
+  T = exifdump.TIFF_file(data)
+  L = T.list_IFDs()
+  for i in range(len(L)):
+    exif_off = None
+    IFD = T.dump_IFD(L[i])
+    for (tag,type,values) in IFD:
+      if tag == 0x8769:
+        exif_off = values[0]
+      elif tag in exifdump.EXIF_TAGS:
+        stag = exifdump.EXIF_TAGS[tag]
+        if stag == "FocalLength": fl = values[0].num
+    if exif_off and not fl:
+      IFD = T.dump_IFD(exif_off)
+      for (tag,type,values) in IFD:
+        if tag in exifdump.EXIF_TAGS:
+          stag = exifdump.EXIF_TAGS[tag]
+          if stag == "FocalLength":  
+            fl = values[0].num
+          elif stag == "DateTimeOriginal": dt = values
+  f.close()
+  if fl > 60: fl=75
+  return (fl,dt)
+
+def lens_name(focal_length):
+  if focal_length == 12: lens = "Olympus M.Zuiko 12mm f/2"
+  elif focal_length == 25: lens = "Panasonic Leica DG Summilux 25mm f/1.4"
+  elif focal_length == 60: lens = "Olympus M.Zuiko 60mm f/2.8 Macro"
+  else: lens = "UNKNOWN"
+  return lens
+
+def image_fname(datetime,fname):
+  """
+  >>> image_fname("2013-11-11","~/kuvat/jpg/2013/11/PB123456.jpg")
+  '/images/2013/11/PB123456_c.jpg'
+  """
+  year = datetime[:4]
+  month = datetime[5:7]
+  fname = fname[fname.rfind('/'):fname.rfind('.')]
+  return "/images/%s/%s%s_c.jpg" % (year,month,fname)
+
+def post_fname(datetime,subject):
+  """
+  >>> post_fname("2013-11-11","Foo bar")
+  '2013-11-11-foo-bar.markdown'
+  """
+  fname = subject.replace(' ','-').lower()
+  fname = "%s-%s.markdown" % (datetime,fname)
+  return fname
+
+if __name__ == '__main__':
+  img_fname = sys.argv[1]
+  subject = sys.argv[2]
+  dir = sys.argv[3]
+  focal_length,datetime = process_file(img_fname)
+  if not datetime: print "### Datetime missing from image (%s)" % (img_fname)
+  if not focal_length: print "### Focal length missing from image (%s)" % (img_fname)
+  if not datetime or not focal_length: sys.exit(1)
+
+  # print "datetime = " + datetime
+  datetime = datetime[:10].replace(':','-')
+  img_fname = image_fname(datetime,img_fname)
+  lens = lens_name(focal_length)
+
+  days = time.strptime(datetime,"%Y-%m-%d")[7]
+  fname = post_fname(datetime,subject)
+  f = open(fname,"w")
+  f.write('''---
+date: '%s 12:00:00'
+layout: post
+status: publish
+title: %s (%d/365)
+---
+![%s](%s)
+<!--more-->
+%s
+''' % (datetime,subject,days,subject,img_fname,lens))
+  f.close()
+  print "### %s written." % (fname)
+  assets = assets_on_octopress.AssetsFixer()
+  assets.scan(dir)
+  assets.validate()
