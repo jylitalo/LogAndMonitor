484bd76f7e77070858d8b4eb5a1103816ac94879
Juha Ylitalo <juha@ylitalot.net>
2014-01-01 18:14:09 +0200

    refactored padpost to work with 365+ projects

--- python/padpost.py
+++ python/padpost.py
@@ -3,7 +3,7 @@
 import glob
 import os
 import sys
-import time
+import datetime
 
 # on Mac OS X:
 # brew install python
@@ -13,68 +13,90 @@ import exifread
 
 import assets_on_octopress
 
-def process_file(fname):
-  f = open(fname)
-  tags = exifread.process_file(f)
-  f.close()
+class PADpost(assets_on_octopress.AssetsFixer):
+  def __init__(self, start_at, end_at):
+    assets_on_octopress.AssetsFixer.__init__(self)
+    self.__start_at = start_at
+    self.__end_at = end_at
 
-  fl = dt = None
-  if tags.has_key("EXIF DateTimeOriginal"): dt = tags["EXIF DateTimeOriginal"].values
-  if tags.has_key("EXIF FocalLength"): fl = tags["EXIF FocalLength"].values[0].num
+  def _process_file(self,img_name):
+    assert os.access(img_name, os.R_OK), "Need read access on " + img_name
+    f = open(img_name)
+    tags = exifread.process_file(f)
+    f.close()
 
-  errmsg = None
-  if not (dt or fl): errmsg = "DateTime and FocalLength are"
-  elif not dt: errmsg = "DateTime is"
-  elif not fl: errmsg = "FocalLength is"
-  if errmsg: raise AssertionError(errmsg + " missing from " + fname)
+    fl = dt = None
+    if tags.has_key("EXIF DateTimeOriginal"): dt = tags["EXIF DateTimeOriginal"].values
+    if tags.has_key("EXIF FocalLength"): fl = tags["EXIF FocalLength"].values[0].num
 
-  dt = dt[:10].replace(':','-')
-  if fl > 60: fl=75
-  return (fl,dt)
+    errmsg = None
+    if not (dt or fl): errmsg = "DateTime and FocalLength are"
+    elif not dt: errmsg = "DateTime is"
+    elif not fl: errmsg = "FocalLength is"
+    if errmsg: raise AssertionError(errmsg + " missing from " + img_name)
 
-def lens_name(focal_length):
-  lenses = { 12 : "Olympus M.Zuiko 12mm f/2",
-             25 : "Panasonic Leica DG Summilux 25mm f/1.4",
-             60 : "Olympus M.Zuiko 60mm f/2.8 Macro" }
-  if focal_length in lenses: return lenses[focal_length]
-  return "UNKNOWN"
+    dt = dt[:10].replace(':','-')
+    if fl > 60: fl=75
+    return dt,self._lens_name(fl)
 
-def image_fname(datetime,fname):
-  """
-  >>> image_fname("2013-11-11","~/kuvat/jpg/2013/11/PB123456.jpg")
-  '/images/2013/11/PB123456_c.jpg'
-  """
-  year,month = datetime[:4],datetime[5:7]
-  fname = fname[fname.rfind('/'):fname.rfind('.')]
-  return "/images/%s/%s%s_c.jpg" % (year,month,fname)
+  def _post_name(self,taken_date,subject):
+    """
+    +++ PADpost._post_name("foo/source","2013-11-11","Foo bar")
+    'foo/source/_posts/2013-11-11-foo-bar.markdown'
+    """
+    dir = self.find_source_dir()
+    fname = subject.replace(' ','-').lower()
+    fname = "%s/_posts/%s-%s.markdown" % (dir,taken_date,fname)
+    return fname
 
-def post_fname(dir,datetime,subject):
-  """
-  >>> post_fname("foo/source","2013-11-11","Foo bar")
-  'foo/source/_posts/2013-11-11-foo-bar.markdown'
-  """
-  fname = subject.replace(' ','-').lower()
-  fname = "%s/_posts/%s-%s.markdown" % (dir,datetime,fname)
-  return fname
+  def post(self,img_name,subject):
+    taken_date, lens = self._process_file(img_name)
 
+    days_done = self._days(self.__start_at,taken_date)
+    days_total = self._days(self.__start_at,self.__end_at)
+    title = "%s (%d/%d)" % (subject,days_done,days_total)
+
+    f = open(self._post_name(taken_date,subject),"w")
+    f.write(self.head(taken_date + " 12:00:00",'post',title))
+
+    url = self._img_url(taken_date,img_name)
+    f.write('''
+![%s](%s)
+<!--more-->
+%s
+''' % (subject,url,lens))
+    f.close()
+
+  @staticmethod
+  def _days(start_at, end_at):
+    start_t = datetime.datetime.strptime(start_at,"%Y-%m-%d")
+    end_t = datetime.datetime.strptime(end_at,"%Y-%m-%d")
+    return (end_t-start_t).days+1
+
+  @staticmethod
+  def _lens_name(value):
+    lenses = { 12 : "Olympus M.Zuiko 12mm f/2",
+               25 : "Panasonic Leica DG Summilux 25mm f/1.4",
+               60 : "Olympus M.Zuiko 60mm f/2.8 Macro" }
+    if value in lenses: return lenses[value]
+    else:
+      raise AssertionError("Unable to map focal length (%s) to lens" % (value))
+
+  @staticmethod
+  def _img_url(taken_date, img_name):
+    """
+    >>> PADpost._img_url("2013-11-11","~/kuvat/jpg/2013/11/PB123456.jpg")
+    '/images/2013/11/PB123456_c.jpg'
+    """
+    year,month = taken_date[:4],taken_date[5:7]
+    fname = img_name[img_name.rfind('/'):img_name.rfind('.')]
+    return "/images/%s/%s%s_c.jpg" % (year,month,fname)
+ 
 if __name__ == '__main__':
   img_fname = sys.argv[1]
   subject = sys.argv[2]
 
-  focal_length,datetime = process_file(img_fname)
-  img_fname = image_fname(datetime,img_fname)
-  lens = lens_name(focal_length)
-
-  days = time.strptime(datetime,"%Y-%m-%d")[7]
-  title = "%s (%d/365)" % (subject,days)
-  assets = assets_on_octopress.AssetsFixer()
-  fname = post_fname(assets.find_source_dir(),datetime,subject)
-  f = open(fname,"w")
-  f.write('''%s![%s](%s)
-<!--more-->
-%s
-''' % (assets.head(datetime + " 12:00:00",'post',title),subject,img_fname,lens))
-  f.close()
-  print "### %s written." % (fname)
-  assets.scan()
-  assets.validate()
+  pad = PADpost("2013-01-01","2014-12-31")
+  pad.post(img_fname,subject)
+  pad.scan()
+  pad.validate()
